4c67edb29f1d701bb071f751b07fd972ce6dfc0f
Maurice Svay <maurice@svay.com>
2013-10-08 21:21:41 +0200

    Use underscore template for ShowView

--- app/views/show-view.js
+++ app/views/show-view.js
@@ -6,18 +6,30 @@ $(function () {
 		tagName : 'li',
 		events: {
 		},
+		initialize: function() {
+			this.tpl = _.template($('#tpl-show-view').text());
+		},
 		render: function renderPicView() {
 			var titleSize = "";
 			var titleLength = this.model.get('title').length;
 			if (titleLength > 26) {
 				titleSize = "smaller";
 			}
-			$(this.el).html(
-				"<span class='channel'><img src='css/images/" + this.model.get('channel').replace(/[^A-Za-z0-9]/,'').toLowerCase() + ".png' alt='" + this.model.get('channel') + "' width='100' height='100'></span> " +
-				"<div class='info'><strong class='title " + titleSize + "'>" + this.model.get('title') + "</strong> " +
-				"<span class='time'>" + this.model.get('time') + "</span> " +
-				"<div class='description'>" + /*this.model.get('description')*/'' + "</div></div>"
-			);
+
+			var image_prefix = "css/images/";
+			var image_suffix = ".png";
+			//@FIXME : remove accents and special chars
+			var image = image_prefix + this.model.get('channel').replace(/[^A-Za-z0-9]/,'').toLowerCase() + image_suffix;
+
+			var out = this.tpl({
+				image: image,
+				channel: this.model.get('channel'),
+				titleSize : titleSize,
+				title : this.model.get('title'),
+				time : this.model.get('time'),
+				description: '' //this.model.get('description')
+			});
+			$(this.el).html(out);
 			return this;
 		}
 	});
